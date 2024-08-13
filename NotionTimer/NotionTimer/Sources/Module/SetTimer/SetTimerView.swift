3fc9961a864a69daf8b08315ce3b23f64d91d7e5
Taichone <taichone.dev@gmail.com>
2024-08-13 21:46:41 +0900

    feat: :construction:  SetTimerView の UI

--- NotionTimer/NotionTimer/Sources/Module/SetTimer/SetTimerView.swift
+++ NotionTimer/NotionTimer/Sources/Module/SetTimer/SetTimerView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SetTimerView: View {
+    @State private var isBreakEndAlarmEnabled = false
+    @State private var isManualBreakStartEnabled = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Notion Settings") {
+                    // TODO: TagSlectionView に遷移
+                    Text("タグ >")
+                }
+                
+                Section("Timer Settings") {
+                    // TODO: TimeSelectionView に遷移
+                    Text("集中時間 >")
+                    // TODO: TimeSelectionView に遷移
+                    Text("休憩時間 >")
+                    Toggle(isOn: self.$isBreakEndAlarmEnabled) {
+                        Text("Trigger an alarm at break end")
+                    }
+                    Toggle(isOn: self.$isManualBreakStartEnabled) {
+                        Text("Start break time manually")
+                    }
+                }
+                
+                Button {
+                    // TODO: TimerView に遷移して開始
+                    print("===Start Timer")
+                } label: {
+                    Text("Start Timer").bold()
+                }
+            }
+            .navigationTitle("Set Timer")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        // TODO: 設定に遷移（制限するアプリの設定, Notion アカウント関連など？）
+                        print("===Setting")
+                    } label: {
+                        Image(systemName: "gearshape.fill")
+                    }
+                }
+            }
+        }
     }
 }
 
