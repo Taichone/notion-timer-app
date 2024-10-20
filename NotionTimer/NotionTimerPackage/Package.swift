41df1a92fff74b39153b062ac0cda962726c08bd
Taichone <taichone.dev@gmail.com>
2024-10-20 15:38:11 +0900

    refactor: モジュール名の変更 - notion を records に

--- NotionTimer/NotionTimerPackage/Package.swift
+++ NotionTimer/NotionTimerPackage/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Notion",
+            name: "Records",
             dependencies: []
         ),
         .target(
