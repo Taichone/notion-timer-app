5421e1014cff8b993879522618cca825d0b57c30
Taichone <taichone.dev@gmail.com>
2024-11-18 20:17:46 +0900

    refactor: NotionSwift への移行完了

--- NotionTimer/NotionTimerPackage/Sources/Notion/NotionService.swift
+++ NotionTimer/NotionTimerPackage/Sources/Notion/NotionService.swift
@@ -19,10 +19,11 @@ public enum NotionAuthStatus {
 public enum NotionServiceError: Error {
     case failedToSaveToKeychain
     case failedToFetchAccessToken
-    case failedToCreateDatabase
     case accessTokenNotFound
-    case failedToGetPageList
-    case failedToGetDatabaseList
+    case invalidClient
+    case failedToGetPageList(error: Error)
+    case failedToGetDatabaseList(error: Error)
+    case failedToCreateDatabase(error: Error)
 }
 
 @MainActor
@@ -50,7 +51,6 @@ public enum NotionServiceError: Error {
                 throw NotionServiceError.failedToSaveToKeychain
             }
             
-            notionClient = NotionClient(accessKeyProvider: StringAccessKeyProvider(accessKey: accessToken))
             fetchAuthStatus()
         } catch {
             authStatus = .invalidToken
@@ -59,20 +59,20 @@ public enum NotionServiceError: Error {
     }
     
     public func fetchAuthStatus() {
-        // TODO: 有効チェック
-        
-        // -[] accessToken 有効チェック
-        guard accessToken != nil else {
+        guard let accessToken = accessToken else {
             authStatus = .invalidToken
+            notionClient = nil
             return
         }
         
-        // -[] databaseID 有効チェック
+        notionClient = NotionClient(accessKeyProvider: StringAccessKeyProvider(accessKey: accessToken))
+        
         guard databaseID != nil else {
             authStatus = .invalidDatabase
             return
         }
         
+        // TODO: token, databaseID の有効チェック
         authStatus = .complete
     }
     
@@ -92,59 +92,135 @@ public enum NotionServiceError: Error {
         
         authStatus = .invalidDatabase
     }
-    
-    // MARK:  Page
-    
+}
+
+// MARK: NotionSwift
+
+extension NotionService {
     public func getPageList() async throws -> [PageEntity] {
-        var resultPages: Result<[Page], NotionClientError>
-        
-        // TODO: Concurrency ラッピング
-        notionClient?.search(request: .init(filter: .page)) { result in
-            resultPages = result.map { objects in
-                objects.results.compactMap({ object -> Page? in
-                    if case .page(let page) = object {
-                        return page
-                    }
-                    return nil
-                })
-            }
+        guard let notionClient = notionClient else {
+            throw NotionServiceError.invalidClient
         }
         
-        let pages = try resultPages.get()
-        return pages.compactMap { $0.asPageEntity }
+        do {
+            return try await getPageList(client: notionClient)
+        } catch {
+            throw NotionServiceError.failedToGetPageList(error: error)
+        }
     }
-    
-    // MARK: Database
         
     public func getDatabaseList() async throws -> [DatabaseEntity] {
-        var resultDatabases: Result<[Database], NotionClientError>
-        
-        // TODO: Concurrency ラッピング
-        notionClient?.search(request: .init(filter: .database)) { result in
-            resultDatabases = result.map { objects in
-                objects.results.compactMap({ object -> Database? in
-                    if case .database(let db) = object {
-                        return db
-                    }
-                    return nil
-                })
-            }
+        guard let notionClient = notionClient else {
+            throw NotionServiceError.invalidClient
         }
         
-        let databases = try resultDatabases.get()
-        return databases.compactMap { $0.asDatabaseEntity }
+        do {
+            return try await getDatabaseList(client: notionClient)
+        } catch {
+            throw NotionServiceError.failedToGetDatabaseList(error: error)
+        }
     }
     
     public func createDatabase(parentPageID: String, title: String) async throws {
-        guard let accessToken = accessToken else {
-            throw NotionServiceError.accessTokenNotFound
+        guard let notionClient = notionClient else {
+            throw NotionServiceError.invalidClient
         }
         
-        let databaseID = try await NotionAPIClient.createDatabase(accessToken: accessToken, parentPageID: parentPageID, title: title)
-        guard KeychainManager.saveToken(token: databaseID, type: .notionDatabaseID) else {
-            throw NotionServiceError.failedToSaveToKeychain
+        do {
+            let databaseID = try await createDatabaseAndGetDatabaseID(
+                parentPageID: parentPageID,
+                title: title,
+                client: notionClient
+            )
+            
+            // Keychain にデータベースIDを保存し、認証状態を更新
+            guard KeychainManager.saveToken(token: databaseID, type: .notionDatabaseID) else {
+                throw NotionServiceError.failedToSaveToKeychain
+            }
+            authStatus = .complete
+        } catch {
+            throw NotionServiceError.failedToCreateDatabase(error: error)
         }
-        authStatus = .complete
+    }
+}
+
+extension NotionService {
+    private func createDatabaseAndGetDatabaseID(
+        parentPageID: String,
+        title: String,
+        client: NotionClient
+    ) async throws -> String {
+        let request = DatabaseCreateRequest(
+            parent: .pageId(.init(parentPageID)),
+            icon: .none,
+            cover: .none,
+            title: [
+                .init(string: title)
+            ],
+            properties: [
+                "Date": .date,
+                "Title": .title,
+                "Tag": .multiSelect([]),
+                "Time": .number(.numberWithCommas),
+                "Description": .richText,
+            ]
+        )
+
+        return try await withCheckedThrowingContinuation { continuation in
+            client.databaseCreate(request: request) { result in
+                switch result {
+                case .success(let db):
+                    continuation.resume(returning: db.id.rawValue)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                    debugPrint(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func getDatabaseList(client: NotionClient) async throws -> [DatabaseEntity] {
+        return try await withCheckedThrowingContinuation { continuation in
+            client.search(request: .init(filter: .database)) { result in
+                let resultDatabases = result.map { objects in
+                    objects.results.compactMap({ object -> Database? in
+                        if case .database(let db) = object {
+                            return db
+                        }
+                        return nil
+                    })
+                }
+                
+                do {
+                    let databases = try resultDatabases.get().compactMap { $0.asDatabaseEntity }
+                    continuation.resume(returning: databases)
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    private func getPageList(client: NotionClient) async throws -> [PageEntity] {
+        return try await withCheckedThrowingContinuation { continuation in
+            client.search(request: .init(filter: .page)) { result in
+                let resultPages = result.map { objects in
+                    objects.results.compactMap({ object -> Page? in
+                        if case .page(let page) = object {
+                            return page
+                        }
+                        return nil
+                    })
+                }
+                
+                do {
+                    let pages = try resultPages.get().compactMap { $0.asPageEntity }
+                    continuation.resume(returning: pages)
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
     }
 }
 
