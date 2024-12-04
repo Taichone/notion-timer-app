8ac2aef19b556f4988f8f3df57c121270a7dc4fa
Taichone <taichone.dev@gmail.com>
2024-12-04 16:30:21 +0900

    update: グラフの UI 修正

--- NotionTimer/NotionTimerPackage/Sources/Timer/Presentation/RecordDisplayView.swift
+++ NotionTimer/NotionTimerPackage/Sources/Timer/Presentation/RecordDisplayView.swift
@@ -8,56 +8,67 @@
 import SwiftUI
 import Charts
 import Notion
+import Common
 
 struct RecordDisplayView: View {
     @Environment(NotionService.self) private var notionService
     @State private var records: [RecordEntity] = []
+    @State private var isLoading = true
+    private let chartViewID = UUID()
     
     var body: some View {
-        ScrollViewReader { proxy in
-            ScrollView(.horizontal) {
-                Chart {
-                    ForEach(records) { record in
-                        BarMark(
-                            x: .value("Date", record.date, unit: .day),
-                            y: .value("Time", record.time)
-                        )
-                        .foregroundStyle(LinearGradient(
-                            gradient: Gradient(
-                                colors: record.tags.map { $0.color.color }
-                            ),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        ))
+        ZStack {
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal) {
+                    Chart {
+                        ForEach(records) { record in
+                            BarMark(
+                                x: .value("Date", record.date, unit: .day),
+                                y: .value("Time", record.time)
+                            )
+                            .foregroundStyle(LinearGradient(
+                                gradient: Gradient(
+                                    colors: record.tags.map { $0.color.color }
+                                ),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            ))
+                        }
                     }
+                    .chartXAxis {
+                        AxisMarks(values: .stride(by: .day))
+                    }
+                    .frame(height: 200)
+                    .padding()
+                    .frame(width: chartViewWidth)
+                    .id(chartViewID)
                 }
-                .chartXAxis {
-                    AxisMarks(values: .stride(by: .day))
-                }
-                .frame(height: 200)
-                .padding()
-                .frame(minWidth: scrollViewMinWidth)
-            }
-            .onAppear {
-                if let today = records.map({ $0.date }).max() {
-                    proxy.scrollTo(today, anchor: .trailing)
-                }
-            }
-            .task {
-                do {
-                    records = try await notionService.getAllRecords()
-                } catch {
-                    debugPrint("ERROR: 記録の取得に失敗") // TODO: ハンドリング
+                .task {
+                    await fetchAllRecords()
+                    proxy.scrollTo(chartViewID, anchor: .trailing) // 右端へスクロール
                 }
             }
+            
+            CommonLoadingView()
+                .hidden(!isLoading)
         }
     }
     
-    private var scrollViewMinWidth: CGFloat {
+    private var chartViewWidth: CGFloat {
         let uniqueDates = Set(records.map { record in
             Calendar.current.startOfDay(for: record.date)
         })
         
-        return CGFloat(uniqueDates.count * 80)
+        return CGFloat(uniqueDates.count * 100)
+    }
+    
+    private func fetchAllRecords() async {
+        do {
+            isLoading = true
+            records = try await notionService.getAllRecords()
+            isLoading = false
+        } catch {
+            debugPrint("ERROR: 記録の取得に失敗") // TODO: ハンドリング
+        }
     }
 }
